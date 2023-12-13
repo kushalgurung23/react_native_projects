afe32da0331c1f6731a51d6a0135f4667ed4943e
Kushalgurung23 <qoosal23.kg@gmail.com>
2023-12-13 17:45:42 +0800

    Working on project 7

---
+++ currency_06/src/components/CurrencyButton.tsx
@@ -0,0 +1,34 @@
+import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+
+type CurrencyButtonProps = PropsWithChildren<{
+    name: string,
+    flag: string
+}>
+
+const CurrencyButton = (props: CurrencyButtonProps): JSX.Element => {
+    return (
+        <View style = {styles.buttonContainer}>
+            <Text style = {styles.flag}>{props.flag}</Text>
+            <Text style = {styles.country}>{props.name}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        alignItems: 'center'
+    },
+    flag: {
+        fontSize: 28,
+        color: 'white',
+        marginBottom: 4
+    },
+    country: {
+        fontSize: 14,
+        color: 'grey',
+    }
+})
+
+export default CurrencyButton
---
+++ tic_tac_toe_07/src/components/Icons.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { PropsWithChildren } from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+type IconProps = PropsWithChildren<{
+    name: string;
+}>
+
+const Icons = ({name}: IconProps) => {
+    switch (name) {
+        case 'circle':
+            return <Icon name = 'circle-thin' size={38}
+            color='#F7CD2E' />
+            
+        case 'cross':
+            return <Icon name = 'times' size={38}
+            color='#38CC77' />
+        
+        default:
+            return <Icon name = 'circle-thin' size={38}/>
+    }
+}
+export default Icons
